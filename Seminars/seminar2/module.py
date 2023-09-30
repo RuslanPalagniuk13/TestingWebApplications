7b6b20e7c1daa88dc9133a16f76a19b867b54560
ruslan13 <rlpalagniuk@mail.ru>
2023-09-30 18:01:49 +0300

    семинар 2

--- Seminars/seminar2/module.py
+++ Seminars/seminar2/module.py
@@ -3,8 +3,8 @@ import yaml
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver.chrome import ChromeDriverManager
+from webdriver.firefox import GeckoDriverManager
 
 with open("./testdata.yaml") as f:
     testdata = yaml.safe_load(f)
